1a08618e5e601d248029384bc892ceba69a6f304
Canaan-HS <s3459897@gmail.com>
2025-04-22 16:53:58 +0800

    修正適應 AJAX 換頁創建 (待優化)

--- KemerDownloader/KemerDownloader-Dev.js
+++ KemerDownloader/KemerDownloader-Dev.js
@@ -31,6 +31,7 @@
 
 // @run-at       document-start
 // @grant        window.close
+// @grant        window.onurlchange
 // @grant        GM_info
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -1239,13 +1240,10 @@
 
     (new class Main {
         constructor() {
-            this.URL = Syn.$url;
-            this.Page = {
-                Content: /^(https?:\/\/)?(www\.)?.+\/.+\/user\/.+\/post\/.+$/.test(this.URL),
-                Preview: /^(https?:\/\/)?(www\.)?.+\/posts\/?(\?.*)?$/.test(this.URL)
-                    || /^(https?:\/\/)?(www\.)?.+\/.+\/user\/[^\/]+(\?.*)?$/.test(this.URL)
-                    || /^(https?:\/\/)?(www\.)?.+\/dms\/?(\?.*)?$/.test(this.URL)
-            }
+            this.Content = (URL) => /^(https?:\/\/)?(www\.)?.+\/.+\/user\/.+\/post\/.+$/.test(URL),
+            this.Preview = (URL) => /^(https?:\/\/)?(www\.)?.+\/posts\/?(\?.*)?$/.test(URL)
+                || /^(https?:\/\/)?(www\.)?.+\/.+\/user\/[^\/]+(\?.*)?$/.test(URL)
+                || /^(https?:\/\/)?(www\.)?.+\/dms\/?(\?.*)?$/.test(URL)
 
             this.AddStyle = async () => {
                 Syn.AddStyle(`
@@ -1286,48 +1284,44 @@
 
         /* 按鈕創建 */
         async ButtonCreation() {
-            Syn.$q("section").$sAttr("Download-Button-Created", true); this.AddStyle();
-            let Button, Files;
-            const IntervalFind = setInterval(() => {
-                Files = Syn.$qa(IsNeko ? "div.scrape__body h2" : "div.post__body h2");
+            this.AddStyle();
 
-                if (Files.length > 0) {
-                    clearInterval(IntervalFind);
+            Syn.WaitElem(IsNeko ? "div.scrape__body h2" : "div.post__body h2", null, {all: true, timeout: 5}).then(Files => {
+                let Button;
 
-                    try {
-                        const CompressMode = Syn.Local("Compression", { error: true });
-                        const ModeDisplay = CompressMode ? Lang.Transl("壓縮下載") : Lang.Transl("單圖下載");
+                try {
+                    const CompressMode = Syn.Local("Compression", { error: true });
+                    const ModeDisplay = CompressMode ? Lang.Transl("壓縮下載") : Lang.Transl("單圖下載");
 
-                        // 創建 Span (找到含有 Files 文本的對象)
-                        Files = Array.from(Files).filter(file => file.$text() == "Files");
-                        if (Files.length == 0) {
-                            return;
-                        }
+                    // 創建 Span (找到含有 Files 文本的對象)
+                    Files = Array.from(Files).filter(file => file.$text() == "Files");
+                    if (Files.length == 0) return;
+                    Syn.$q("#ExDB")?.remove(); // 非必要, 避免意外
 
-                        const spanElement = Syn.$createElement(Files[0], "span", { class: "File_Span", id: "ExDB" });
-                        // 創建 Svg
-                        const setting = Syn.$createElement(spanElement, "svg", { class: "Setting_Button" });
-                        setting.$iHtml(`<svg xmlns="http://www.w3.org/2000/svg" height="1.3rem" viewBox="0 0 512 512"><style>svg {fill: hsl(0, 0%, 45%);}</style>
-                        <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>`
-                        );
-                        setting.$one("click", () => { alert("Currently Invalid") }, { capture: true, passive: true });
-                        // 創建 Button
-                        Button = Syn.$createElement(spanElement, "button", {
-                            class: "Download_Button",
-                            text: lock ? Lang.Transl("下載中鎖定") : ModeDisplay
-                        });
-                        Button.disabled = lock;
-                        Button.$one("click", () => {
-                            let Instantiate = null;
-                            Instantiate = new Download(CompressMode, ModeDisplay, Button);
-                            Instantiate.DownloadTrigger();
-                        }, { capture: true, passive: true });
-                    } catch {
-                        Button.disabled = true;
-                        Button.$text(Lang.Transl("無法下載"));
-                    }
+                    const spanElement = Syn.$createElement(Files[0], "span", { class: "File_Span", id: "ExDB" });
+                    const setting = Syn.$createElement(spanElement, "svg", { class: "Setting_Button" });
+                    setting.$iHtml(`<svg xmlns="http://www.w3.org/2000/svg" height="1.3rem" viewBox="0 0 512 512"><style>svg {fill: hsl(0, 0%, 45%);}</style>
+                    <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>`
+                    );
+                    setting.$one("click", () => { alert("Currently Invalid") }, { capture: true, passive: true });
+
+                    // 創建 Button
+                    Button = Syn.$createElement(spanElement, "button", {
+                        class: "Download_Button",
+                        text: lock ? Lang.Transl("下載中鎖定") : ModeDisplay
+                    });
+                    Button.disabled = lock;
+
+                    Button.$one("click", () => {
+                        let Instantiate = null;
+                        Instantiate = new Download(CompressMode, ModeDisplay, Button);
+                        Instantiate.DownloadTrigger();
+                    }, { capture: true, passive: true });
+                } catch {
+                    Button.disabled = true;
+                    Button.$text(Lang.Transl("無法下載"));
                 }
-            });
+            })
         }
 
         /* 一鍵開啟當前所有帖子 */
@@ -1354,38 +1348,56 @@
             Syn.Local("Compression", { error: true })
                 ? Syn.Local("Compression", { value: false })
                 : Syn.Local("Compression", { value: true });
-            Syn.$q("#ExDB").remove();
+            Syn.$q("#ExDB")?.remove();
             this.ButtonCreation();
         }
 
         /* 注入檢測創建 [ 檢測頁面創建按鈕, 創建菜單 ] */
         async Injection() {
-            Syn.Observer(document, () => {
-                try {
-                    (this.Page.Content && !Syn.$q("section").$hAttr("Download-Button-Created")) && this.ButtonCreation();
-                } catch { }
-            }, { throttle: 300 });
+            const self = this;
 
-            if (this.Page.Content) {
-                Syn.Menu({
-                    [Lang.Transl("🔁 切換下載模式")]: { func: () => this.DownloadModeSwitch(), close: false, hotkey: "c" }
-                });
+            // 首次載入嘗試註冊
+            registerMenu(Syn.$url);
+            self.Content(Syn.$url) && self.ButtonCreation();
 
-            } else if (this.Page.Preview) {
-                Syn.Menu({
-                    [Lang.Transl("📑 獲取帖子數據")]: {
-                        func: () => {
-                            if (!lock) {
-                                let Instantiate = null;
-                                Instantiate = new FetchData(FetchSet.AdvancedFetch, FetchSet.ToLinkTxt);
-                                FetchSet.UseFormat && Instantiate.FetchConfig(FetchSet.Mode, FetchSet.Format);
-                                Instantiate.FetchInit();
+            /* 注銷菜單 */
+            async function unregisterMenu() {
+                for (let i = 1; i <= 2; i++) GM_unregisterMenuCommand("Menu-" + i);
+            };
+
+            /* 註冊菜單 */
+            async function registerMenu(Page) {
+
+                if (self.Content(Page)) {
+                    unregisterMenu();
+
+                    Syn.Menu({
+                        [Lang.Transl("🔁 切換下載模式")]: { func: () => self.DownloadModeSwitch(), close: false, hotkey: "c" }
+                    });
+                } else if (self.Preview(Page)) {
+                    unregisterMenu();
+
+                    Syn.Menu({
+                        [Lang.Transl("📑 獲取帖子數據")]: {
+                            func: () => {
+                                if (!lock) {
+                                    let Instantiate = null;
+                                    Instantiate = new FetchData(FetchSet.AdvancedFetch, FetchSet.ToLinkTxt);
+                                    FetchSet.UseFormat && Instantiate.FetchConfig(FetchSet.Mode, FetchSet.Format);
+                                    Instantiate.FetchInit();
+                                }
                             }
-                        }
-                    },
-                    [Lang.Transl("📃 開啟當前頁面帖子")]: { func: () => this.OpenAllPages() }
-                });
-            }
+                        },
+                        [Lang.Transl("📃 開啟當前頁面帖子")]: { func: () => self.OpenAllPages() }
+                    });
+                }
+            };
+
+            window.$one("urlchange", change => {
+                self.Content(change.url) && self.ButtonCreation();
+                registerMenu(change.url);
+            });
+
         }
     }).Injection();
 
