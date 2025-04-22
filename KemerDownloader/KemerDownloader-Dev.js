b02b8e2453d2d8008838d944194bc30ea32c1903
Canaan-HS <s3459897@gmail.com>
2025-04-23 05:41:00 +0800

    調整按鈕創建

--- KemerDownloader/KemerDownloader-Dev.js
+++ KemerDownloader/KemerDownloader-Dev.js
@@ -72,7 +72,7 @@
 
 (async () => {
     const Config = {
-        Dev: false, // 顯示請求資訊, 與錯誤資訊
+        Dev: true, // 顯示請求資訊, 與錯誤資訊
         ContainsVideo: false, // 下載時包含影片
         CompleteClose: false, // 下載完成後關閉
         ConcurrentDelay: 3, // 下載線程延遲 (秒) [壓縮下載]
@@ -411,8 +411,9 @@
                 compression: "DEFLATE",
                 compressionOptions: { level: 5 }
             }, (progress) => {
-                Syn.$title(`${progress.percent.toFixed(1)} %`);
-                this.Button.$text(`${Lang.Transl("封裝進度")}: ${progress.percent.toFixed(1)} %`);
+                const display = `${progress.percent.toFixed(1)} %`;
+                Syn.$title(display);
+                this.Button.$text(`${Lang.Transl("封裝進度")}: ${display}`);
             }).then(zip => {
                 saveAs(zip, `${Name}.zip`);
                 Syn.$title(`✓ ${Title}`);
@@ -541,7 +542,7 @@
         async ResetButton() {
             Config.CompleteClose && window.close();
             lock = false;
-            const Button = Syn.$q("#ExDB button");
+            const Button = Syn.$q("#File_Span button");
             Button.disabled = false;
             Button.$text(`✓ ${this.ModeDisplay}`);
         }
@@ -1248,9 +1249,12 @@
             this.AddStyle = async () => {
                 Syn.AddStyle(`
                     ${GM_getResourceText("font-awesome")}
-                    .File_Span {
+                    #File_Span {
                         padding: 1rem;
-                        font-size: 70% !important;
+                        font-size: 40% !important;
+                    }
+                    #File_Span svg {
+                        fill: white;
                     }
                     .Setting_Button {
                         cursor: pointer;
@@ -1260,6 +1264,7 @@
                         padding: 6px;
                         margin: 10px;
                         border-radius: 8px;
+                        font-size: 1.1vw;
                         border: 2px solid rgba(59, 62, 68, 0.7);
                         background-color: rgba(29, 31, 32, 0.8);
                         font-family: "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
@@ -1276,48 +1281,52 @@
                     }
                 `, "Download-button-style", false);
             }
-
-            // 下載模式 native, disabled, browser
-            GM_info.downloadMode = "browser";
-            GM_info.isIncognito = true;
         }
 
         /* 按鈕創建 */
         async ButtonCreation() {
             this.AddStyle();
 
-            Syn.WaitElem(IsNeko ? "div.scrape__body h2" : "div.post__body h2", null, {all: true, timeout: 5}).then(Files => {
-                let Button;
+            Syn.WaitElem(".post__body h2, .scrape__body h2", null, {raf: true, all: true}).then(Files => {
+                Syn.$q("#File_Span")?.remove();
 
+                let Button;
                 try {
+                    // 創建 Span (找到含有 Files 文本的對象)
+                    Files = [...Files].filter(file => file.$text() === "Files");
+                    if (Files.length == 0) return;
+
                     const CompressMode = Syn.Local("Compression", { error: true });
                     const ModeDisplay = CompressMode ? Lang.Transl("壓縮下載") : Lang.Transl("單圖下載");
 
-                    // 創建 Span (找到含有 Files 文本的對象)
-                    Files = Array.from(Files).filter(file => file.$text() == "Files");
-                    if (Files.length == 0) return;
-                    Syn.$q("#ExDB")?.remove(); // 非必要, 避免意外
+                    const spanElement = Syn.$createElement("span", { id: "File_Span" });
 
-                    const spanElement = Syn.$createElement(Files[0], "span", { class: "File_Span", id: "ExDB" });
-                    const setting = Syn.$createElement(spanElement, "svg", { class: "Setting_Button" });
-                    setting.$iHtml(`<svg xmlns="http://www.w3.org/2000/svg" height="1.3rem" viewBox="0 0 512 512"><style>svg {fill: hsl(0, 0%, 45%);}</style>
-                    <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>`
-                    );
-                    setting.$one("click", () => { alert("Currently Invalid") }, { capture: true, passive: true });
-
-                    // 創建 Button
-                    Button = Syn.$createElement(spanElement, "button", {
-                        class: "Download_Button",
-                        text: lock ? Lang.Transl("下載中鎖定") : ModeDisplay
+                    Syn.$createElement(spanElement, "svg", { // 創建設置 SVG
+                        class: "Setting_Button",
+                        innerHTML: `<svg xmlns="http://www.w3.org/2000/svg" height="1.3rem" viewBox="0 0 512 512"><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>`
                     });
-                    Button.disabled = lock;
 
-                    Button.$one("click", () => {
-                        let Instantiate = null;
-                        Instantiate = new Download(CompressMode, ModeDisplay, Button);
-                        Instantiate.DownloadTrigger();
+                    Syn.$createElement(spanElement, "button", { // 創建 Button
+                        class: "Download_Button",
+                        text: lock ? Lang.Transl("下載中鎖定") : ModeDisplay,
+                        disabled: lock
+                    });
+
+                    Files[0].appendChild(spanElement);
+                    spanElement.$one("click", event => {
+                        const target = event.target;
+
+                        if (target.tagName === "BUTTON") {
+                            let Instantiate = null;
+                            Instantiate = new Download(CompressMode, ModeDisplay, target);
+                            Instantiate.DownloadTrigger();
+                        } else if (target.closest("svg")) {
+                            alert("Currently Invalid");
+                        }
                     }, { capture: true, passive: true });
-                } catch {
+                } catch (error) {
+                    Syn.Log("Button Creation Failed", error, { dev: Config.Dev, type: "error", collapsed: false });
+
                     Button.disabled = true;
                     Button.$text(Lang.Transl("無法下載"));
                 }
@@ -1348,13 +1357,16 @@
             Syn.Local("Compression", { error: true })
                 ? Syn.Local("Compression", { value: false })
                 : Syn.Local("Compression", { value: true });
-            Syn.$q("#ExDB")?.remove();
             this.ButtonCreation();
         }
 
         /* 注入檢測創建 [ 檢測頁面創建按鈕, 創建菜單 ] */
         async Injection() {
             const self = this;
+
+            // 下載模式 native, disabled, browser
+            GM_info.downloadMode = "browser";
+            GM_info.isIncognito = true;
 
             // 首次載入嘗試註冊
             registerMenu(Syn.$url);
