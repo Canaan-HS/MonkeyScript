b290c73512f7b5a5adb6f410dd5df307f4a58c95
Canaan-HS <s3459897@gmail.com>
2025-04-25 23:01:26 +0800

    API 更新 (待測試)

--- KemerEnhance/KemerEnhance-Dev.js
+++ KemerEnhance/KemerEnhance-Dev.js
@@ -37,7 +37,7 @@
 // @grant        GM_registerMenuCommand
 // @grant        GM_addValueChangeListener
 
-// @require      https://update.greasyfork.org/scripts/487608/1576506/SyntaxLite_min.js
+// @require      https://update.greasyfork.org/scripts/487608/1577559/SyntaxLite_min.js
 
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.14.1/jquery-ui.min.js
