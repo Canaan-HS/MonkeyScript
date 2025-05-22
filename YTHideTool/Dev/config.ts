58adb623540c7b3d50e708612ced51cdeb43b7c1
Canaan-HS <s3459897@gmail.com>
2025-05-22 14:38:39 +0800

    first

---
+++ YTHideTool/Dev/config.ts
@@ -0,0 +1,35 @@
+import { util } from 'vite-plugin-monkey';
+
+const Name = "YTHideTool";
+export default {
+    entry: `./${Name}/Dev/src/main.js`,
+    userscript: {
+        match: [
+            '*://www.youtube.com/*',
+        ],
+        icon: 'https://cdn-icons-png.flaticon.com/512/1383/1383260.png',
+        namespace: 'https://greasyfork.org/users/989635',
+        noframes: '',
+        'run-at': 'document-start',
+        grant: [
+            'window.onurlchange',
+            'GM_setValue',
+            'GM_getValue',
+            'GM_registerMenuCommand',
+            'GM_addValueChangeListener'
+        ],
+        require: [
+            'https://update.greasyfork.org/scripts/487608/1591150/SyntaxLite_min.js',
+
+            util.dataUrl(`window.Syn=Syn`)
+        ]
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                dir: `./${Name}/Dev/dist`,
+                entryFileNames: `${Name}-Dev.js`,
+            },
+        }
+    },
+};
