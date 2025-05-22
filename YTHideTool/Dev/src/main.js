564758c5af833b63c05483663ff29c7ae36da1e7
Canaan-HS <s3459897@gmail.com>
2025-05-22 14:47:01 +0800

    後續開發

---
+++ YTHideTool/Dev/src/main.js
@@ -0,0 +1,15 @@
+import {
+    monkeyWindow,
+    GM_setValue,
+    GM_getValue,
+    GM_registerMenuCommand,
+    GM_addValueChangeListener,
+} from 'vite-plugin-monkey/dist/client';
+const { Syn } = monkeyWindow;
+
+import { Config } from './config.js';
+import Dict from './language.js';
+
+(async () => {
+    
+})();
