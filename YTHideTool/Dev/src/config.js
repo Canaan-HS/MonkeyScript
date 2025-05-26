8c59bf0ad55707dcda1aa3164fb78129f9ae42c1
Canaan-HS <s3459897@gmail.com>
2025-05-26 14:28:43 +0800

    支援移除

--- YTHideTool/Dev/src/config.js
+++ YTHideTool/Dev/src/config.js
@@ -7,13 +7,11 @@ export const Config = {
         MinimaList: k => k.ctrlKey && Config.HotKey.Adapt(k) == "z", // 極簡化
         RecomViewing: k => k.altKey && Config.HotKey.Adapt(k) == "1", // 推薦觀看
         Comment: k => k.altKey && Config.HotKey.Adapt(k) == "2", // 留言區
-        FunctionBar: k => k.altKey && Config.HotKey.Adapt(k) == "3", // 功能區
-        ListDesc: k => k.altKey && Config.HotKey.Adapt(k) == "4" // 播放清單資訊
+        FunctionBar: k => k.altKey && Config.HotKey.Adapt(k) == "3" // 功能區
     }
 };
 
 export const Match = {
     Live: /^(https?:\/\/)www\.youtube\.com\/live\/.*$/, // 直播影片
-    Video: /^(https?:\/\/)www\.youtube\.com\/watch\?v=.+$/, // 影片播放區
-    Playlist: /^(https?:\/\/)www\.youtube\.com\/playlist\?list=.+$/, // 播放清單
+    Video: /^(https?:\/\/)www\.youtube\.com\/watch\?v=.+$/ // 影片播放區
 }
