e71b0a80258bc12de8cf033863bda8f9b88555b2
Canaan-HS <s3459897@gmail.com>
2025-05-28 10:21:17 +0800

    後續測試

---
+++ ColaMangaEnhance/Dev/src/main.js
@@ -0,0 +1,12 @@
+import {
+    monkeyWindow,
+    GM_setValue,
+    GM_getValue
+} from 'vite-plugin-monkey/dist/client';
+const { Syn } = monkeyWindow;
+
+import { Config } from './config.js';
+
+(async () => {
+
+})();
