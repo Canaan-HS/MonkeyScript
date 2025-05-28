c676d0b8b2aabdbf64661e7df8733bcecedd66ec
Canaan-HS <s3459897@gmail.com>
2025-05-29 00:09:29 +0800

    完成基本初始化重構 (待重構自動換頁)

--- ColaMangaEnhance/Dev/src/main.js
+++ ColaMangaEnhance/Dev/src/main.js
@@ -68,8 +68,10 @@ Todo 未來添加
         }
 
         Syn.AddStyle(`
-                html {pointer-events: none !important;}
-                .mh_wrap, span.mh_btn:not(.contact), ${Control.IdList.Iframe} {pointer-events: auto;}
+            div[style*='position'] {display: none !important;}
+            html {pointer-events: none !important;}
+            .mh_wrap, span.mh_btn:not(.contact), ${Control.IdList.Iframe} {pointer-events: auto;}
+
         `, Control.IdList.Block);
 
         // 雖然性能開銷比較高, 但比較不會跳一堆錯誤訊息
@@ -88,7 +90,7 @@ Todo 未來添加
         const Use = Config.RegisterHotkey.Function;
         let JumpState = false; // 如果不是最後一頁, 觸發時他將會被設置為 true, 反之始終為 false (是 false 才能觸發跳轉, 一個跳轉的防抖機制)
 
-        if (Syn.Platform() === "Desktop") {
+        if (Syn.Platform === "Desktop") {
             // 是主頁, 且啟用保持滾動, 也啟用自動滾動(避免無法手動停止), 且沒有開啟手動滾動(避免無法手動停止)
             if (Param.IsMainPage && Use.KeepScroll && Use.AutoScroll && !Use.ManualScroll) {
                 Param.IsMainPage = tools.Storage("scroll"); // 取得是否有保持滾動
@@ -100,56 +102,57 @@ Todo 未來添加
 
             Syn.onEvent(window, "keydown", event => {
                 const key = event.key;
-                if (key == "ArrowLeft" && Use.TurnPage && !JumpState) {
+
+                if (key === "ArrowLeft" && Use.TurnPage && !JumpState) {
                     event.stopImmediatePropagation();
                     JumpState = !tools.FinalPage(Param.PreviousPage);
                     location.assign(Param.PreviousPage);
                 }
-                else if (key == "ArrowRight" && Use.TurnPage && !JumpState) {
+                else if (key === "ArrowRight" && Use.TurnPage && !JumpState) {
                     event.stopImmediatePropagation();
                     JumpState = !tools.FinalPage(Param.NextPage);
                     location.assign(Param.NextPage);
                 }
-                else if (key == "ArrowUp" && CanScroll) {
+                else if (key === "ArrowUp" && CanScroll) {
                     event.stopImmediatePropagation();
                     event.preventDefault();
 
                     if (Use.ManualScroll) {
-                        tools.ManualScroll(-Syn.iH());
+                        tools.ManualScroll(-Syn.iH);
                     } else {
-                        if (Param.IsMainPage) {
-                            Param.IsMainPage = false;
-                        } else if (!Param.IsMainPage || Param.IsMainPage) {
-                            Param.IsMainPage = false;
-                            Param.IsMainPage = true;
+                        if (Param.Up_scroll) {
+                            Param.Up_scroll = false;
+                        } else if (!Param.Up_scroll || Param.Down_scroll) {
+                            Param.Down_scroll = false;
+                            Param.Up_scroll = true;
                             tools.AutoScroll(UP_ScrollSpeed);
                         }
                     }
                 }
-                else if (key == "ArrowDown" && CanScroll) {
+                else if (key === "ArrowDown" && CanScroll) {
                     event.stopImmediatePropagation();
                     event.preventDefault();
 
                     if (Use.ManualScroll) {
-                        tools.ManualScroll(Syn.iH());
+                        tools.ManualScroll(Syn.iH);
                     } else {
-                        if (Param.IsMainPage) {
-                            Param.IsMainPage = false;
+                        if (Param.Down_scroll) {
+                            Param.Down_scroll = false;
                             tools.Storage("scroll", false);
-                        } else if (Param.IsMainPage || !Param.IsMainPage) {
-                            Param.IsMainPage = false;
-                            Param.IsMainPage = true;
+                        } else if (Param.Up_scroll || !Param.Down_scroll) {
+                            Param.Up_scroll = false;
+                            Param.Down_scroll = true;
                             tools.Storage("scroll", true);
                             tools.AutoScroll(Control.ScrollPixels);
                         }
                     }
                 }
             }, { capture: true });
-        } else if (Syn.Platform() === "Mobile") {
+        } else if (Syn.Platform === "Mobile") {
 
             let startX, startY, moveX, moveY;
-            const sidelineX = Syn.iW() * .3;
-            const sidelineY = (Syn.iH() / 4) * .3;
+            const sidelineX = Syn.iW * .3;
+            const sidelineY = (Syn.iH / 4) * .3;
 
             Syn.onEvent(window, "touchstart", event => {
                 startX = event.touches[0].clientX;
@@ -172,5 +175,58 @@ Todo 未來添加
             }, 60), { passive: true });
         }
     };
+
+    (() => {
+
+        /* 初始化取得數據 */
+        async function Init(callback) {
+            Syn.WaitElem(["body", "div.mh_readtitle", "div.mh_headpager", "div.mh_readend", "#mangalist"], null,
+                { timeout: 10, throttle: 30, timeoutResult: true })
+                .then(([Body, Title, HeadPager, Readend, Manga]) => {
+                    Param.Body = Body;
+
+                    const HomeLink = Title.$qa("a");
+                    Param.ContentsPage = HomeLink[0].href; // 目錄連結
+                    Param.HomePage = HomeLink[1].href; // 首頁連結
+
+                    try {
+                        const PageLink = Readend.$qa("ul a");
+                        Param.PreviousPage = PageLink[0].href;
+                        Param.NextPage = PageLink[2].href;
+                    } catch {
+                        const PageLink = HeadPager.$qa("a.mh_btn:not(.mh_bgcolor)");
+                        Param.PreviousPage = PageLink[0].href;
+                        Param.NextPage = PageLink[1].href;
+                    }
+
+                    Param.MangaList = Manga; // 漫畫列表
+                    Param.BottomStrip = Readend.$q("a"); // 以閱讀完畢的那條, 看到他跳轉
+
+                    if ([
+                        Param.Body,
+                        Param.ContentsPage,
+                        Param.HomePage,
+                        Param.PreviousPage,
+                        Param.NextPage,
+                        Param.MangaList,
+                        Param.BottomStrip
+                    ].every(Check => Check)) callback(true);
+                    else callback(false);
+                });
+        };
+
+        BlockAds();
+
+        try {
+            Init(state => {
+                if (state) { // 在這邊載入的功能都是需要等到, 找到元素才操作比較不會出錯
+                    style.PictureStyle();
+                    Config.BGColor.Enable && style.BackgroundStyle(Config.BGColor.Color);
+                    // Config.AutoTurnPage.Enable && AutoPageTurn(Config.AutoTurnPage.Mode);
+                    Config.RegisterHotkey.Enable && HotkeySwitch();
+                } else Syn.Log(null, "Error");
+            });
+        } catch (error) { Syn.Log(null, error) }
+    })();
 
 })();
