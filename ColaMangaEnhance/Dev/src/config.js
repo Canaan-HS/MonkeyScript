27d98f6a1212a6314baed3d66242f6df940d8cb8
Canaan-HS <s3459897@gmail.com>
2025-05-28 22:55:56 +0800

    配置添加

--- ColaMangaEnhance/Dev/src/config.js
+++ ColaMangaEnhance/Dev/src/config.js
@@ -1,3 +1,4 @@
+/* 臨時的自定義 (當 Enable = false 時, 其餘的設置將無效) */
 export const Config = {
     BGColor: {
         Enable: true,
@@ -16,4 +17,37 @@ export const Config = {
             ManualScroll: false, // 手動滾動啟用時, 將會變成點擊一次, 根據視點翻一頁 且 自動滾動會無效
         }
     }
+};
+
+export const Control = {
+    ScrollPixels: 2, // 像素, 越大越快
+    WaitPicture: 1000, // 載入延遲時間 (ms)
+    BlockListener: new Set([ // 阻擋事件
+        "auxclick",
+        "mousedown",
+        "pointerup",
+        "pointerdown",
+        "dState",
+        "touchstart",
+        "unhandledrejection"
+    ]),
+    IdList: { // Id 表
+        Title: "CME_Title", Iframe: "CME_Iframe", Block: "CME_Block-Ads", Menu: "CME_Menu-Style",
+        Image: "CME_Image-Style", Scroll: "CME_Scroll-Hidden", ChildS: "CME_Child-Scroll-Hidden"
+    }
+};
+
+export const Param = {
+    Body: null, // body 緩存
+    ContentsPage: null, // 返回目錄連結 緩存
+    HomePage: null, // 返回首頁連結 緩存
+    PreviousPage: null, // 上一頁連結 緩存
+    NextPage: null, // 下一頁連結 緩存
+    MangaList: null, // 漫畫列表 緩存
+    BottomStrip: null, // 以閱讀完畢的那條
+    Observer_Next: null, // 下一頁觀察器
+    Up_scroll: false, // 向上滾動判斷值
+    Down_scroll: false, // 向下滾動判斷值
+    IsFinalPage: false, // 最終頁判斷值
+    IsMainPage: (window.self === window.parent) // 判斷是否是 iframe
 };
