810daeff91b476b6b5ae3ad42a83ef681ef93578
Canaan-HS <s3459897@gmail.com>
2025-06-09 03:57:54 +0800

    重新編譯測試

--- ColaMangaEnhance/Dev/dist/ColaMangaEnhance-Dev.js
+++ ColaMangaEnhance/Dev/dist/ColaMangaEnhance-Dev.js
@@ -344,8 +344,8 @@
         };
         if (tools.FinalPage(Param2.NextLink)) {
           window.parent.postMessage([{ StopResize: true }], Syn2.$origin);
-          window.parent.postMessage([{ Recover: true }], Syn2.$origin);
           StylelRules[0].style.display = "block";
+          window.parent.postMessage([{ Recover: true }], Syn2.$origin);
           Resize(40);
           return;
         }
@@ -353,27 +353,24 @@
         Waitload();
         Param2.Body.appendChild(iframe);
         function Waitload() {
-          let IframeWindow, CurrentUrl, Content, AllImg, Completed = false;
+          let IframeWindow, CurrentUrl, Content, AllImg;
           const Failed = () => {
-            Completed = false;
             iframe.offAll();
             iframe.src = Param2.NextLink;
             Waitload();
           };
           const Success = () => {
-            if (Completed) return;
-            Completed = true;
             iframe.offAll();
-            IframeWindow ?? (IframeWindow = iframe.contentWindow);
-            CurrentUrl ?? (CurrentUrl = IframeWindow.location.href);
+            IframeWindow = iframe.contentWindow;
+            CurrentUrl = IframeWindow.location.href;
             if (CurrentUrl !== Param2.NextLink) {
               Failed();
               return;
             }
-            Content ?? (Content = IframeWindow.document);
+            Content = IframeWindow.document;
             Content.body.style.overflow = "hidden";
             Syn2.Log("無盡翻頁", CurrentUrl);
-            AllImg ?? (AllImg = Content.$qa("#mangalist img"));
+            AllImg = Content.$qa("#mangalist img");
             const UrlUpdate = new IntersectionObserver((observed) => {
               observed.forEach((entry) => {
                 var _a2, _b2;
@@ -411,38 +408,8 @@
               AllImg.forEach(async (img) => ReleaseMemory.observe(img));
             }
           };
-          const WaitIframe = () => {
-            const QueryWindow = setInterval(() => {
-              IframeWindow = iframe == null ? void 0 : iframe.contentWindow;
-              if (Completed) {
-                clearInterval(QueryWindow);
-                return;
-              }
-              if (IframeWindow && IframeWindow.document.readyState === "complete") {
-                clearInterval(QueryWindow);
-                CurrentUrl = IframeWindow.location.href;
-                if (CurrentUrl !== Param2.NextLink) {
-                  Failed();
-                  return;
-                }
-                Content = IframeWindow.document;
-                const QueryImg = setInterval(() => {
-                  if (Completed) {
-                    clearInterval(QueryImg);
-                    return;
-                  }
-                  AllImg = Content.$qa("#mangalist img");
-                  if (AllImg.length > 0) {
-                    clearInterval(QueryImg);
-                    Success();
-                  }
-                }, 300);
-              }
-            }, 500);
-          };
           iframe.on("load", Success);
           iframe.on("error", Failed);
-          WaitIframe();
         }
       }
     }
