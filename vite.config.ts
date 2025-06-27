414a7be278205be89937f683a3f1b2245cc12206
Canaan-HS <s3459897@gmail.com>
2025-06-27 20:26:36 +0800

    開發變更

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
 
 // 要開發的對應配置
-import config from './ColaMangaEnhance/Dev/config.ts';
+import config from './ExDownloader/Dev/config.ts';
 
 const entry: string = config.entry;
 const build: object = config.build;
