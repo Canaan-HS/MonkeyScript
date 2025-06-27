428cdaf3bf8651c00f635e20474eb00582f75e79
Canaan-HS <s3459897@gmail.com>
2025-06-27 20:26:48 +0800

    Lib 更新

--- ExDownloader/Dev/config.ts
+++ ExDownloader/Dev/config.ts
@@ -10,7 +10,7 @@ export default {
         namespace: 'https://greasyfork.org/users/989635',
         'run-at': 'document-body',
         require: [
-            'https://update.greasyfork.org/scripts/495339/1580133/Syntax_min.js',
+            'https://update.greasyfork.org/scripts/495339/1613824/Syntax_min.js',
             'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
 
             util.dataUrl(`window.Syn=Syn`)
