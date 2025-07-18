471c6fe8fb691f6fd43357044bb31410d0f53ff3
Canaan-HS <s3459897@gmail.com>
2025-07-18 22:20:40 +0800

    修改導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
 import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
-
-// 要開發的對應配置
-import config from './KemerDownloader/Dev/config.ts';
-
-const entry: string = config.entry;
-const build: object = config.build;
-const userscript: object = config.userscript;
+import config from './KemerDownloader/Dev/config';
 
 export default defineConfig({
-    build,
+    build: {
+        outDir: config.outDir,
+    },
     server: {
         host: '0.0.0.0'
     },
     plugins: [
         monkey({
-            entry,
+            entry: config.entry,
             userscript: {
                 name: 'Vite Dev',
                 version: '0.0.0',
                 author: 'Canaan HS',
                 description: 'Vite Server',
-                ...userscript
+                ...config.userscript
             },
+            build: {
+                fileName: config.fileName,
+            }
         }),
     ],
 });
