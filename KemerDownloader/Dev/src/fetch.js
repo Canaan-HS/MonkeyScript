fd1abf6efcd65577027733ef0711a16c4755f8dc
Canaan-HS <s3459897@gmail.com>
2025-08-05 06:22:36 +0800

    測試只有圖片類型, 可用 filename

--- KemerDownloader/Dev/src/fetch.js
+++ KemerDownloader/Dev/src/fetch.js
@@ -167,15 +167,14 @@ export default function Fetch(
                             link.download?.trim() || link.$text()
                         );
 
-                        // ? 影片不支援 filename= IDM 解碼, 該網站本身沒有 Content-Disposition
                         if (this.isVideo(extension)) {
                             acc.video[getDownloadName(file)] = url;
                         } else if (this.isImage(extension)) {
+                            // ? 僅有圖片支援 ?filename= IDM 解碼, 該網站本身沒有 Content-Disposition
                             const name = `${title}_${String(++imgNumber).padStart(2, "0")}.${extension}`;
                             acc.img[name] = `${url}?filename=${name}`;
                         } else {
-                            const name = getDownloadName(file);
-                            acc.other[name] = `${url}?filename=${name}`;
+                            acc.other[getDownloadName(file)] = url;
                         }
                     }
 
