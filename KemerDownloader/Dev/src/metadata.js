a2e436a1750bee61fc5257891c8fa247f140f9cf
Canaan-HS <s3459897@gmail.com>
2025-08-06 20:43:31 +0800

    版本更新

--- KemerDownloader/Dev/src/metadata.js
+++ KemerDownloader/Dev/src/metadata.js
@@ -8,7 +8,7 @@ export default `
 // @name:ru      Kemer Загрузчик
 // @name:ko      Kemer 다운로더
 // @name:en      Kemer Downloader
-// @version      0.0.21-Beta8
+// @version      2025.08.06-Beta
 // @author       Canaan HS
 // @description         一鍵下載圖片 (壓縮下載/單圖下載) , 一鍵獲取帖子數據以 Json 或 Txt 下載 , 一鍵開啟當前所有帖子
 // @description:zh-TW   一鍵下載圖片 (壓縮下載/單圖下載) , 下載頁面數據 , 一鍵開啟當前所有帖子
