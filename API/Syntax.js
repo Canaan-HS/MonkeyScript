f43e14f5712bf327bf0c5709c2aca77aafa642e5
Canaan-HS <s3459897@gmail.com>
2025-08-23 00:47:37 +0800

    日期格式修改

--- API/Syntax.js
+++ API/Syntax.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Syntax
-// @version      2025/08/23
+// @version      2025.08.23
 // @author       Canaan HS
 // @description  Library for simplifying code logic and syntax
 // @namespace    https://greasyfork.org/users/989635
