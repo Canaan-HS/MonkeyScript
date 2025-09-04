4eb2b243e07c68e20e4584d1e790534cdfa6d8c3
Canaan-HS <s3459897@gmail.com>
2025-09-04 21:22:25 +0800

    update

--- ExDownloader/Dev/config.ts
+++ ExDownloader/Dev/config.ts
@@ -12,10 +12,10 @@ export default {
         namespace: 'https://greasyfork.org/users/989635',
         'run-at': 'document-body',
         require: [
-            'https://update.greasyfork.org/scripts/495339/1616381/Syntax_min.js',
+            'https://update.greasyfork.org/scripts/495339/1654307/Syntax_min.js',
             'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
 
-            util.dataUrl(`window.Syn=Syn`)
+            util.dataUrl(`window.Lib=Lib`)
         ],
     },
 };
