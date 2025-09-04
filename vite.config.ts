2a6ea7a8e29ebf20b6a29a8115485d3d647cd995
Canaan-HS <s3459897@gmail.com>
2025-09-04 21:21:15 +0800

    開發變更

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
-import config from './KemerDownloader/Dev/config.ts';
+import config from './ExDownloader/Dev/config.ts';
 
 export default defineConfig({
     build: {
