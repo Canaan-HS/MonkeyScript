51856a04b3c59deeb3d02f1e36239a6032c4ce1c
Canaan-HS <s3459897@gmail.com>
2025-09-12 23:27:59 +0800

    metadata

---
+++ YTHideTool/Dev/metadata.ts
@@ -0,0 +1,37 @@
+export default `
+
+// ==UserScript==
+// @name         YouTube Hide Tool
+// @name:zh-TW   YouTube 隱藏工具
+// @name:zh-CN   YouTube 隐藏工具
+// @name:ja      YouTube 非表示ツール
+// @name:ko      유튜브 숨기기 도구
+// @name:en      Youtube Hide Tool
+// @version      0.0.38-Beta1
+// @author       Canaan HS
+// @description         該腳本能夠自動隱藏 YouTube 影片結尾的推薦卡，當滑鼠懸浮於影片上方時，推薦卡會恢復顯示。並額外提供快捷鍵切換功能，可隱藏留言區、影片推薦、功能列表，及切換至極簡模式。設置會自動保存，並在下次開啟影片時自動套用。
+// @description:zh-TW   該腳本能夠自動隱藏 YouTube 影片結尾的推薦卡，當滑鼠懸浮於影片上方時，推薦卡會恢復顯示。並額外提供快捷鍵切換功能，可隱藏留言區、影片推薦、功能列表，及切換至極簡模式。設置會自動保存，並在下次開啟影片時自動套用。
+// @description:zh-CN   该脚本能够自动隐藏 YouTube 视频结尾的推荐卡，在鼠标悬停于视频上方时，推荐卡会恢复显示。并额外提供快捷键切换功能，可隐藏评论区、视频推荐、功能列表，并切换至极简模式。设置会自动保存，并在下次打开视频时自动应用。
+// @description:ja      このスクリプトは、YouTube动画の终わりに表示される推奨カードを自动的に非表示にし、マウスがビデオ上にホバーされると、推奨カードが再表示されます。さらに、ショートカットキーでコメントセクション、动画の推奨、机能リストを非表示に切り替える机能が追加されており、シンプルモードに切り替えることもできます。设定は自动的に保存され、次回ビデオを开くと自动的に适用されます。
+// @description:ko      이 스크립트는 YouTube 동영상 끝에 나오는 추천 카드를 자동으로 숨기고, 마우스가 비디오 위에 머무를 때 추천 카드가 다시 나타납니다. 또한, 댓글 섹션, 비디오 추천, 기능 목록을 숨기고 최소 모드로 전환하는 단축키를 제공합니다. 설정은 자동으로 저장되며, 다음 비디오를 열 때 자동으로 적용됩니다.
+// @description:en      This script automatically hides the recommended cards at the end of YouTube videos. When the mouse hovers over the video, the recommended cards will reappear. Additionally, it provides shortcut keys to toggle the comment section, video recommendations, feature list, and switch to a minimalist mode. Settings are automatically saved and applied the next time the video is opened.
+
+// @noframes
+// @match        *://www.youtube.com/*
+// @icon         https://cdn-icons-png.flaticon.com/512/1383/1383260.png
+
+// @license      MPL-2.0
+// @namespace    https://greasyfork.org/users/989635
+
+// @require      https://update.greasyfork.org/scripts/487608/1591150/SyntaxLite_min.js
+
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        window.onurlchange
+// @grant        GM_registerMenuCommand
+// @grant        GM_addValueChangeListener
+
+// @run-at       document-start
+// ==/UserScript==
+
+`
