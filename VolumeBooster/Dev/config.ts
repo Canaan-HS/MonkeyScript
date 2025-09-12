9dae34d6c5cc887a2a4e11097b74dfb49b4ed7c7
Canaan-HS <s3459897@gmail.com>
2025-09-13 00:02:22 +0800

    配置更新

--- VolumeBooster/Dev/config.ts
+++ VolumeBooster/Dev/config.ts
@@ -1,33 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { util } from 'vite-plugin-monkey';
 
-const Name = "VolumeBooster";
-export default {
-    entry: `./${Name}/Dev/src/main.js`,
-    fileName: `${Name}-Dev.js`,
-    outDir: `./${Name}/Dev/dist`,
-    userscript: {
-        match: [
-            '*://*/*',
-        ],
-        icon: 'https://cdn-icons-png.flaticon.com/512/16108/16108408.png',
-        namespace: 'https://greasyfork.org/users/989635',
-        noframes: '',
-        'run-at': 'document-body',
-        grant: [
-            'GM_setValue',
-            'GM_getValue',
-            'GM_deleteValue',
-            'GM_getResourceURL',
-            'GM_registerMenuCommand',
-            'GM_addValueChangeListener'
-        ],
-        resource: {
-            'Img': 'https://cdn-icons-png.flaticon.com/512/11243/11243783.png',
-        },
-        require: [
-            'https://update.greasyfork.org/scripts/487608/1637584/SyntaxLite_min.js',
+import metaData from './metadata';
+import parseMeta from '../../parseMeta';
 
-            util.dataUrl(`window.Lib=Lib`)
-        ]
+const meta = metaData.trim();
+const parsed = parseMeta(meta);
+const name = path.basename(path.resolve(fileURLToPath(import.meta.url), '../../'));
+
+export default {
+    meta,
+    entry: `./${name}/Dev/src/main.js`,
+    fileName: `${name}-Dev.js`,
+    outDir: `./${name}/Dev/dist`,
+    userscript: {
+        ...parsed.basic,
+        require: [
+            ...parsed.require,
+            util.dataUrl('window.Lib=Lib')
+        ],
     },
 };
