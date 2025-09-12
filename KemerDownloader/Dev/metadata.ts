20e6a18d13c1f31ff284dde7d0536e473159532e
Canaan-HS <s3459897@gmail.com>
2025-09-13 00:02:44 +0800

    metadata

---
+++ KemerDownloader/Dev/metadata.ts
@@ -0,0 +1,49 @@
+export default `
+
+// ==UserScript==
+// @name         Kemer Downloader
+// @name:zh-TW   Kemer 下載器
+// @name:zh-CN   Kemer 下载器
+// @name:ja      Kemer ダウンローダー
+// @name:ru      Kemer Загрузчик
+// @name:ko      Kemer 다운로더
+// @name:en      Kemer Downloader
+// @version      2025.09.03-Beta
+// @author       Canaan HS
+// @description         一鍵下載圖片 (壓縮下載/單圖下載) , 一鍵獲取帖子數據以 Json 或 Txt 下載 , 一鍵開啟當前所有帖子
+// @description:zh-TW   一鍵下載圖片 (壓縮下載/單圖下載) , 下載頁面數據 , 一鍵開啟當前所有帖子
+// @description:zh-CN   一键下载图片 (压缩下载/单图下载) , 下载页面数据 , 一键开启当前所有帖子
+// @description:ja      画像をワンクリックでダウンロード（圧縮ダウンロード/単一画像ダウンロード）、ページデータを作成してjsonでダウンロード、現在のすべての投稿をワンクリックで開く
+// @description:ru      Загрузка изображений в один клик (сжатая загрузка/загрузка отдельных изображений), создание данных страницы для загрузки в формате json, открытие всех текущих постов одним кликом
+// @description:ko      이미지 원클릭 다운로드(압축 다운로드/단일 이미지 다운로드), 페이지 데이터 생성하여 json 다운로드, 현재 모든 게시물 원클릭 열기
+// @description:en      One-click download of images (compressed download/single image download), create page data for json download, one-click open all current posts
+
+// @connect      *
+// @match        *://kemono.cr/*
+// @match        *://coomer.st/*
+// @match        *://nekohouse.su/*
+
+// @license      MPL-2.0
+// @namespace    https://greasyfork.org/users/989635
+// @supportURL   https://github.com/Canaan-HS/MonkeyScript/issues
+// @icon         https://cdn-icons-png.flaticon.com/512/2381/2381981.png
+
+// @require      https://update.greasyfork.org/scripts/495339/1654307/Syntax_min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.19.0/js/md5.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
+
+// @grant        window.close
+// @grant        window.onurlchange
+// @grant        GM_info
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_download
+// @grant        GM_openInTab
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+
+// @run-at       document-start
+// ==/UserScript==
+
+`
