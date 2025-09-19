6a6507c0e471719e172d545c1344a74f9d106d6f
Canaan-HS <s3459897@gmail.com>
2025-09-20 00:22:26 +0800

    變更參數解構位置

--- ColaMangaEnhance/Dev/src/features/hotkey.js
+++ ColaMangaEnhance/Dev/src/features/hotkey.js
@@ -4,8 +4,7 @@ import { Config, Control, Param } from '../core/config.js';
 import Tools from '../utils/tools.js';
 
 /* 快捷切換上下頁 和 自動滾動 */
-export default async () => {
-    const { TurnPage, AutoScroll, KeepScroll, ManualScroll } = Config.RegisterHotkey.Function;
+export default async ({ TurnPage, AutoScroll, KeepScroll, ManualScroll } = Config.RegisterHotkey.Function) => {
 
     let jumpState = false; // 如果不是最後一頁, 觸發時他將會被設置為 true, 反之始終為 false (是 false 才能觸發跳轉, 一個跳轉的防抖機制)
     if (Lib.platform === "Desktop") {
