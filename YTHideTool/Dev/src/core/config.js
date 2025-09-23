8ea8f8756933691e8ae1273638d4c25e9b9ea536
Canaan-HS <s3459897@gmail.com>
2025-09-23 19:35:20 +0800

    config

---
+++ YTHideTool/Dev/src/core/config.js
@@ -0,0 +1,27 @@
+import { Lib } from '../services/client';
+
+export const Config = {
+    Dev: false,
+    GlobalChange: true, // 全局同時修改
+    HotKey: {
+        Adapt: k => k.key.toLowerCase(), // <- 適配大小寫差異
+        Title: k => k.altKey && Config.HotKey.Adapt(k) == "t", // 標題
+        MinimaList: k => k.ctrlKey && Config.HotKey.Adapt(k) == "z", // 極簡化
+        RecomViewing: k => k.altKey && Config.HotKey.Adapt(k) == "1", // 推薦觀看
+        Comment: k => k.altKey && Config.HotKey.Adapt(k) == "2", // 留言區
+        FunctionBar: k => k.altKey && Config.HotKey.Adapt(k) == "3" // 功能區
+    },
+    ...Lib.getV('__REMOVE_ON_BUILD__', {})
+};
+
+export const Match = {
+    Live: /^(https?:\/\/)www\.youtube\.com\/live\/.*$/, // 直播影片
+    Video: /^(https?:\/\/)www\.youtube\.com\/watch\?v=.+$/, // 影片播放區
+    ...Lib.getV('__REMOVE_ON_BUILD__', {})
+}
+
+export const Param = {
+    Token: false,
+    StartTime: undefined,
+    ...Lib.getV('__REMOVE_ON_BUILD__', {})
+}
