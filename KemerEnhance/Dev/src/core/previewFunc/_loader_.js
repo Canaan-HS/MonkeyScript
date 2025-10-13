9e752a7c717e621d5e22fc5939953f0f014f9d47
Canaan-HS <s3459897@gmail.com>
2025-10-13 18:22:19 +0800

    preview loader

---
+++ KemerEnhance/Dev/src/core/previewFunc/_loader_.js
@@ -0,0 +1,19 @@
+import NewTabOpens from './NewTabOpens.js';
+import CardText from './CardText.js';
+import CardZoom from './CardZoom.js';
+import QuickPostToggle from './QuickPostToggle.js';
+import BetterThumbnailFactory from './BetterThumbnail.js';
+
+const previewLoader = {
+    NewTabOpens,
+    CardText,
+    CardZoom,
+    async BetterThumbnail(...args) {
+        const value = BetterThumbnailFactory().BetterThumbnail;
+        Object.defineProperty(this, value.name, { value, writable: false });
+        value(...args);
+    },
+    QuickPostToggle
+};
+
+export default previewLoader;
