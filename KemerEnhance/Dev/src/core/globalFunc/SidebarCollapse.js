a979514e982db8b47a90a5b76ac1efd9c3249b32
Canaan-HS <s3459897@gmail.com>
2025-10-13 18:21:26 +0800

    SidebarCollapse

---
+++ KemerEnhance/Dev/src/core/globalFunc/SidebarCollapse.js
@@ -0,0 +1,24 @@
+import { Lib } from '../../services/client.js';
+
+/* 收縮側邊攔 */
+export default async function SidebarCollapse() {
+    if (Lib.platform === "Mobile") return;
+
+    Lib.addStyle(`
+        .global-sidebar {
+            opacity: 0;
+            height: 100%;
+            width: 10rem;
+            display: flex;
+            position: fixed;
+            padding: 0.5em 0;
+            transition: 0.8s;
+            background: #282a2e;
+            flex-direction: column;
+            transform: translateX(-9rem);
+        }
+        .global-sidebar:hover {opacity: 1; transform: translateX(0rem);}
+        .content-wrapper.shifted {transition: 0.7s; margin-left: 0rem;}
+        .global-sidebar:hover + .content-wrapper.shifted {margin-left: 10rem;}
+    `, "Collapse-Effects", false);
+};
