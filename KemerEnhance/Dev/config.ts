420c5136597e7d3339b0b96fdbba233146a000dd
Canaan-HS <s3459897@gmail.com>
2025-10-13 18:18:07 +0800

    config

---
+++ KemerEnhance/Dev/config.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { util } from 'vite-plugin-monkey';
+
+import metaData from './metadata';
+import parseMeta from '../../parseMeta';
+
+const meta = metaData.trim();
+const parsed = parseMeta(meta);
+
+const appName = path.basename(path.resolve(fileURLToPath(import.meta.url), '../../'));
+
+export default {
+    meta,
+    entry: `./${appName}/Dev/src/entry/bootstrap.js`,
+    devFileName: `${appName}.user.js`,
+    devOutDir: `./${appName}/Dev/dist`,
+    releaseFileName: `${appName}.user.js`,
+    releaseOutDir: `./${appName}/Release`,
+    userscript: {
+        ...parsed.basic,
+        require: [
+            ...parsed.require,
+            util.dataUrl('window.Lib=Lib'),
+            util.dataUrl('window.preact=preact')
+        ],
+    },
+};
