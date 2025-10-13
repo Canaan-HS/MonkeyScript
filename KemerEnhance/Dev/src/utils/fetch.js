22e3bfdc51ded1203c23d5bb698d926d51d8377a
Canaan-HS <s3459897@gmail.com>
2025-10-13 18:20:17 +0800

    fetch

---
+++ KemerEnhance/Dev/src/utils/fetch.js
@@ -0,0 +1,62 @@
+import { Lib } from '../services/client.js';
+
+const Fetch = (() => {
+    const responseRule = {
+        text: res => res.text(),
+        json: res => res.json(),
+        blob: res => res.blob(),
+        arrayBuffer: res => res.arrayBuffer(),
+        formData: res => res.formData(),
+        document: async res => {
+            res = await res.text();
+            return Lib.domParse(res);
+        },
+    };
+
+    const fetchRecord = {};
+    const abort = (url) => {
+        fetchRecord[url]?.abort();
+        delete fetchRecord[url];
+    };
+
+    async function send(url, callback, {
+        responseType = "json",
+        headers = { "Accept": "text/css" }
+    } = {}) {
+        try {
+            fetchRecord[url]?.abort();
+        } catch { }
+
+        const controller = new AbortController();
+        fetchRecord[url] = controller;
+
+        return new Promise((resolve, reject) => {
+            fetch(url, { headers, signal: controller.signal })
+                .then(async response => {
+                    if (!response.ok) {
+                        const text = await response.text();
+                        throw new Error(`\nFetch failed\nurl: ${response.url}\nstatus: ${response.status}\nstatusText: ${text}`);
+                    }
+
+                    try {
+                        return await responseRule[responseType](response);
+                    } catch { }
+                })
+                .then(res => {
+                    resolve(res);
+                    callback?.(res);
+                })
+                .catch(error => {
+                    reject(error);
+                    Lib.log(error).error;
+                })
+                .finally(() => {
+                    delete fetchRecord[url];
+                });
+        })
+    };
+
+    return { abort, send }
+})();
+
+export default Fetch;
