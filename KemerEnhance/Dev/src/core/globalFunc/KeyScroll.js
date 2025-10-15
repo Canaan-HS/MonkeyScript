87b0a13cf879324ab39a1a13a08e998c6fd1147f
Canaan-HS <s3459897@gmail.com>
2025-10-15 19:53:54 +0800

    命名調整

--- KemerEnhance/Dev/src/core/globalFunc/KeyScroll.js
+++ KemerEnhance/Dev/src/core/globalFunc/KeyScroll.js
@@ -5,52 +5,52 @@ export default async function KeyScroll({ mode }) { /* 快捷自動滾動 */
     if (Lib.platform === "Mobile" || Parame.Registered.has("KeyScroll")) return;
 
     // 滾動配置
-    const Scroll_Requ = {
-        Scroll_Pixels: 2,
-        Scroll_Interval: 800,
+    const scrollConfig = {
+        scrollPixel: 2,
+        scrollInterval: 800,
     };
 
-    const UP_ScrollSpeed = Scroll_Requ.Scroll_Pixels * -1;
-    let Scroll, Up_scroll = false, Down_scroll = false;
+    const upScrollSpeed = scrollConfig.scrollPixel * -1;
+    let scrollFunc, isUpScroll = false, isDownScroll = false;
 
-    const [TopDetected, BottomDetected] = [ // 到頂 和 到底 的檢測
+    const [topDetected, bottomDetected] = [ // 到頂 和 到底 的檢測
         Lib.$throttle(() => {
-            Up_scroll = Lib.sY == 0
+            isUpScroll = Lib.sY == 0
                 ? false : true
         }, 600),
         Lib.$throttle(() => {
-            Down_scroll = Lib.sY + Lib.iH >= Lib.html.scrollHeight
+            isDownScroll = Lib.sY + Lib.iH >= Lib.html.scrollHeight
                 ? false : true
         }, 600)
     ];
 
     switch (mode) {
         case 2:
-            Scroll = (Move) => {
+            scrollFunc = (Move) => {
                 const Interval = setInterval(() => {
-                    if (!Up_scroll && !Down_scroll) {
+                    if (!isUpScroll && !isDownScroll) {
                         clearInterval(Interval);
                     }
 
-                    if (Up_scroll && Move < 0) {
+                    if (isUpScroll && Move < 0) {
                         window.scrollBy(0, Move);
-                        TopDetected();
-                    } else if (Down_scroll && Move > 0) {
+                        topDetected();
+                    } else if (isDownScroll && Move > 0) {
                         window.scrollBy(0, Move);
-                        BottomDetected();
+                        bottomDetected();
                     }
-                }, Scroll_Requ.Scroll_Interval);
+                }, scrollConfig.scrollInterval);
             }
         default:
-            Scroll = (Move) => {
-                if (Up_scroll && Move < 0) {
+            scrollFunc = (Move) => {
+                if (isUpScroll && Move < 0) {
                     window.scrollBy(0, Move);
-                    TopDetected();
-                    requestAnimationFrame(() => Scroll(Move));
-                } else if (Down_scroll && Move > 0) {
+                    topDetected();
+                    requestAnimationFrame(() => scrollFunc(Move));
+                } else if (isDownScroll && Move > 0) {
                     window.scrollBy(0, Move);
-                    BottomDetected();
-                    requestAnimationFrame(() => Scroll(Move));
+                    bottomDetected();
+                    requestAnimationFrame(() => scrollFunc(Move));
                 }
             }
     }
@@ -60,22 +60,22 @@ export default async function KeyScroll({ mode }) { /* 快捷自動滾動 */
         if (key == "ArrowUp") {
             event.stopImmediatePropagation();
             event.preventDefault();
-            if (Up_scroll) {
-                Up_scroll = false;
-            } else if (!Up_scroll || Down_scroll) {
-                Down_scroll = false;
-                Up_scroll = true;
-                Scroll(UP_ScrollSpeed);
+            if (isUpScroll) {
+                isUpScroll = false;
+            } else if (!isUpScroll || isDownScroll) {
+                isDownScroll = false;
+                isUpScroll = true;
+                scrollFunc(upScrollSpeed);
             }
         } else if (key == "ArrowDown") {
             event.stopImmediatePropagation();
             event.preventDefault();
-            if (Down_scroll) {
-                Down_scroll = false;
-            } else if (Up_scroll || !Down_scroll) {
-                Up_scroll = false;
-                Down_scroll = true;
-                Scroll(Scroll_Requ.Scroll_Pixels);
+            if (isDownScroll) {
+                isDownScroll = false;
+            } else if (isUpScroll || !isDownScroll) {
+                isUpScroll = false;
+                isDownScroll = true;
+                scrollFunc(scrollConfig.scrollPixel);
             }
         }
     }, 100), { capture: true });
