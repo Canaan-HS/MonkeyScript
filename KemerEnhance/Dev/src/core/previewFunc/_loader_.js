1fed39d2da7f883ac9bdec9743a1d1fd5b86958b
Canaan-HS <s3459897@gmail.com>
2026-02-16 15:40:40 +0800

    調整 loader 順序

--- KemerEnhance/Dev/src/core/previewFunc/_loader_.js
+++ KemerEnhance/Dev/src/core/previewFunc/_loader_.js
@@ -1,19 +1,19 @@
-import NewTabOpens from './NewTabOpens.js';
 import CardText from './CardText.js';
 import CardZoom from './CardZoom.js';
+import NewTabOpens from './NewTabOpens.js';
 import QuickPostToggle from './QuickPostToggle.js';
 import BetterThumbnailFactory from './BetterThumbnail.js';
 
 const previewLoader = {
-    NewTabOpens,
     CardText,
     CardZoom,
+    NewTabOpens,
+    QuickPostToggle,
     async BetterThumbnail(...args) {
         const value = BetterThumbnailFactory().BetterThumbnail;
         value(...args);
         Object.defineProperty(this, value.name, { value, writable: false });
     },
-    QuickPostToggle
 };
 
 export default previewLoader;
