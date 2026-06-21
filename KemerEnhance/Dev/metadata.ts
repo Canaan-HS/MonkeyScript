886cc524093728a291267ce0d0c9a4c2e84d4884
Canaan-HS <s3459897@gmail.com>
2026-06-21 21:11:11 +0800

    增加 pawchive (還未適配)

--- KemerEnhance/Dev/metadata.ts
+++ KemerEnhance/Dev/metadata.ts
@@ -21,6 +21,7 @@ export default `
 // @connect      *
 // @match        *://kemono.cr/*
 // @match        *://coomer.st/*
+// @match        *://pawchive.st/*
 // @match        *://nekohouse.su/*
 
 // @license      MPL-2.0
