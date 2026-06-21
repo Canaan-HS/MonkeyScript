26705a8cb9d990aec21dd956337947a172572ae1
Canaan-HS <s3459897@gmail.com>
2026-06-21 19:23:19 +0800

    2026.06.21-Beta

--- ExDownloader/Dev/metadata.ts
+++ ExDownloader/Dev/metadata.ts
@@ -8,7 +8,7 @@ export default `
 // @name:ko      [E/Ex-Hentai] 다운로더
 // @name:ru      [E/Ex-Hentai] Загрузчик
 // @name:en      [E/Ex-Hentai] Downloader
-// @version      2026.05.26-Beta
+// @version      2026.06.21-Beta
 // @author       Canaan HS
 // @description         漫畫頁面創建下載按鈕, 可切換 (壓縮下載 | 單圖下載), 無須複雜設置一鍵點擊下載, 自動獲取(非原圖)進行下載
 // @description:zh-TW   漫畫頁面創建下載按鈕, 可切換 (壓縮下載 | 單圖下載), 無須複雜設置一鍵點擊下載, 自動獲取(非原圖)進行下載
