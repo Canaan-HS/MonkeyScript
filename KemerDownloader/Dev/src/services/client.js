3b6056a83bb0b11ab2198227201d3481a9072bef
Canaan-HS <s3459897@gmail.com>
2026-07-25 19:31:55 +0800

    移除 md5 導入

--- KemerDownloader/Dev/src/services/client.js
+++ KemerDownloader/Dev/src/services/client.js
@@ -1,3 +1,3 @@
 import { monkeyWindow } from 'vite-plugin-monkey/dist/client';
-const { Lib, saveAs, md5 } = monkeyWindow;
-export { monkeyWindow, Lib, saveAs, md5 };
+const { Lib, saveAs } = monkeyWindow;
+export { monkeyWindow, Lib, saveAs };
