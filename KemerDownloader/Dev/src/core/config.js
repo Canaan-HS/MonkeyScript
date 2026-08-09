a5b712312474ad4842b1e29278d9a42e7916960c
Canaan-HS <s3459897@gmail.com>
2026-08-09 18:30:19 +0800

    改為 vite dev 時自動啟用 dev 參數

--- KemerDownloader/Dev/src/core/config.js
+++ KemerDownloader/Dev/src/core/config.js
@@ -1,7 +1,7 @@
 import { Lib } from "../services/client.js";
 
 const General = {
-    Dev: false,            // 顯示請求資訊, 與錯誤資訊
+    Dev: import.meta.env.DEV, // 顯示請求資訊, 與錯誤資訊
     IncludeExtras: false,  // 下載時包含 影片 與 其他附加檔案
     CompleteClose: false,  // 下載完成後關閉
     ConcurrentDelay: 500,  // 下載線程延遲 (ms) [壓縮下載]
