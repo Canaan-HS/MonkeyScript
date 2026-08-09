3b0c98eb5858506fcc04a41d4a6095231613f533
Canaan-HS <s3459897@gmail.com>
2026-08-09 21:17:31 +0800

    適配新配置

--- KemerEnhance/Dev/src/core/previewFunc/NewTabOpens.js
+++ KemerEnhance/Dev/src/core/previewFunc/NewTabOpens.js
@@ -1,15 +1,13 @@
 import { Lib } from '../../services/client.js';
 
 /* 將預覽頁面 開啟帖子都變成新分頁開啟 */
-export default async function NewTabOpens({ newtab_active, newtab_insert }) {
-    const [active, insert] = [newtab_active, newtab_insert];
-
+export default async function NewTabOpens(openInTab) {
     Lib.onEvent(Lib.body, "click", event => {
         const target = event.target.closest("article a");
         target && (
             event.preventDefault(),
             event.stopImmediatePropagation(),
-            GM_openInTab(target.href, { active, insert })
+            GM_openInTab(target.href, openInTab)
         );
     }, { capture: true, mark: "NewTabOpens" });
 }
