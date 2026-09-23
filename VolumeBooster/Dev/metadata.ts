4c67aa91b7d5e73c35c90cece87dc8b0fe5291a1
Canaan-HS <s3459897@gmail.com>
2026-09-24 06:59:25 +0800

    update

--- VolumeBooster/Dev/metadata.ts
+++ VolumeBooster/Dev/metadata.ts
@@ -5,7 +5,7 @@ export default `
 // @name:zh-TW   媒體音量增強器
 // @name:zh-CN   媒体音量增强器
 // @name:en      Media Volume Booster
-// @version      2025.12.12-Beta
+// @version      2026.09.24-Beta
 // @author       Canaan HS
 // @description         調整媒體音量與濾波器，增強倍數最高 20 倍，設置可記住並自動應用。部分網站可能無效、無聲音或無法播放，可選擇禁用。
 // @description:zh-TW   調整媒體音量與濾波器，增強倍數最高 20 倍，設置可記住並自動應用。部分網站可能無效、無聲音或無法播放，可選擇禁用。
@@ -23,6 +23,7 @@ export default `
 // @resource     Icon https://cdn-icons-png.flaticon.com/512/11243/11243783.png
 // @require      https://update.greasyfork.org/scripts/487608/1909139/SyntaxLite_min.js
 
+// @grant        unsafeWindow
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
